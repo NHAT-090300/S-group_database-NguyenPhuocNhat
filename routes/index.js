9e1f6face2749cd6450f89d645455ab753244f09
Your Name <nhatnguyen.090300@gmail.com>
2020-03-23 15:10:12 +0700

    lanmot

---
+++ routes/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+const {
+  renderRegister,
+  renderLogin,
+  renderUser,
+  renderIcon,
+  renderTable,
+  renderMap,
+  renderNotificastion,
+  renderTypography,
+  renderUpgrade,
+  renderRtl
+} = require('../app/User/user.controller');
+const {
+  registerMethod,
+  loginMethod,
+  logOut,
+  userIsAuth,
+  userIsNotAuth,
+  renderHomepage,
+  Delete
+} = require('../app/Admin/controller')
+router.get('/logOut',logOut);
+router.get('/register',userIsAuth,renderRegister);
+router.post('/register',userIsAuth,registerMethod);
+router.get('/login',userIsAuth,renderLogin);
+router.post('/login',userIsAuth,loginMethod);
+router.get('/user',userIsNotAuth,renderUser);
+router.get('/',userIsNotAuth,renderHomepage);
+router.get('/icons',userIsNotAuth,renderIcon);
+router.get('/table',userIsNotAuth,renderTable);
+router.get('/map',userIsNotAuth,renderMap);
+router.get('/notifications',userIsNotAuth,renderNotificastion);
+router.get('/typography',userIsNotAuth,renderTypography);
+router.get('/upgrade',userIsNotAuth,renderUpgrade);
+router.get('/rtl',userIsNotAuth,renderRtl);
+router.get('/table/:id',userIsNotAuth,Delete);
+
+module.exports = router;
